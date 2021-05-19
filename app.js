af8dffa020f19fa22576d55b6d9b09c0549aab14
stefiian <stefilorean@gmail.com>
2021-05-18 21:55:33 -0300

    heroku

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use( express.static(path.resolve (__dirname,"./public")));
 
-app.listen(3020, () => console.log('esto fue exitoso'));
+app.listen(process.env.PORT || 3020, () => console.log('esto fue exitoso'));
 
 app.get('/', (req, res) => {
     res.sendFile (path.resolve (__dirname, "./views/home.html"))
